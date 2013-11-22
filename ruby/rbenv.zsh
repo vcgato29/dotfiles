984e07426b8abb9b7604313b82c7bf1241cffec9
Victor Klos <victor@victorklos.nl>
2013-11-22 15:01:26 +0100

    update rbenv.zsh according to manpage
    Conflicts:
    	ruby/rbenv.zsh

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,24 +1,2 @@
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init - zsh)"
-
-export LANG="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-
-# rehash shims
-rbenv rehash 2>/dev/null
-
-# shell thing
-rbenv() {
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
+# init according to man page
+eval "$(rbenv init -)"
