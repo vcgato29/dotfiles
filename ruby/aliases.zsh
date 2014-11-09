e3210d51baaaca06e011901b01ea03ed2e90a9ba
Piotr Chmolowski <piotr@chmolowski.pl>
2014-11-09 15:22:14 +0100

    I don't need this stuff

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,5 +1,3 @@
-alias f='RAILS_ENV=fi'
-
 alias rg='bundle exec rails generate'
 alias rsv='bundle exec rails server -p `available_rails_port`'
 alias rc='bundle exec rails console'
@@ -10,6 +8,3 @@ alias bu='bundle update'
 
 alias migrate='rake db:migrate db:test:clone'
 
-alias s="ps aux | grep \"[s]cript/rails\" || echo \"You're not running any, dawg.\""
-
-alias watch="watchr ~/.watchr/rails.rb"
