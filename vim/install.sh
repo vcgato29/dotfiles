7266c8a0606025c9e181f16fcc8ab08a455547ca
Piotr Chmolowski <piotr@chmolowski.pl>
2015-09-21 21:55:42 +0200

    Replace command-t with ctrlp, use silversearcher, remove unused plugins

--- vim/install.sh
+++ vim/install.sh
@@ -6,17 +6,9 @@
 
 brew install ctags
 
-function compile_command_t() {
-  cd $HOME/.vim/bundle/command-t/ruby/command-t
-  ruby extconf.rb
-  make
-}
-
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 vim -u $HOME/.vimrc.bundles +PluginInstall +PluginClean! +qa
-
-compile_command_t
 
 exit 0
