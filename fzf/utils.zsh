1ae584ffd34fe73fda1c6b31f5d670ecc7648afd
Piotr Chmolowski <piotr@chmolowski.pl>
2015-11-07 15:15:19 +0100

    fzf utility functions

---
+++ fzf/utils.zsh
@@ -0,0 +1,31 @@
+# fshow - git commit browser (enter for show, ctrl-d for diff, ` toggles sort)
+# source: http://junegunn.kr/2015/03/browsing-git-commits-with-fzf/
+fshow() {
+  local out shas sha q k
+  while out=$(
+    git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --multi --no-sort --reverse --query="$q" \
+      --print-query --expect=ctrl-d); do
+    q=$(head -1 <<< "$out")
+    k=$(head -2 <<< "$out" | tail -1)
+    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
+    [ -z "$shas" ] && continue
+    if [ "$k" = ctrl-d ]; then
+      git diff --color=always $shas | less -R
+    else
+      for sha in $shas; do
+        git show --color=always $sha | less -R
+      done
+    fi
+  done
+}
+
+# Browse & checkout git branches with with fzf
+# source: http://junegunn.kr/2015/03/fzf-tmux/
+fbr() {
+  local branches branch
+  branches=$(git branch) &&
+  branch=$(echo "$branches" | fzf-tmux -d 15 +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //")
+}
