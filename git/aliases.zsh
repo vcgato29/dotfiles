cc8e1ff39a3075499a2fac86ee7c1b12aa937aaf
Piotr Chmolowski <piotrek@pch-mbp.local>
2015-12-12 16:24:50 +0100

    Get rid of .symlink extension

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,21 +1,6 @@
-
 alias gst='git status'
-
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gcb='git copy-branch-name'
-
 alias gb='git branch'
 alias gba='git branch -a'
-
-# source: http://coderwall.com/p/euwpig?i=3&p=1&t=git
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
